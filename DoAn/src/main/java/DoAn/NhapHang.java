5ff79624813d0874fa7404cd3d23b9fc01f8216a
tuantran4792 <tuantran4792@gmail.com>
2016-05-24 14:35:30 +0700

    First commit

---
+++ DoAn/src/main/java/DoAn/NhapHang.java
@@ -0,0 +1,62 @@
+package DoAn;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
+public class NhapHang extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtTenHang;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NhapHang frame = new NhapHang();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NhapHang() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(150, 30, 1000, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTnHng = new JLabel("T\u00EAn h\u00E0ng:");
+		lblTnHng.setBounds(22, 52, 58, 14);
+		contentPane.add(lblTnHng);
+		
+		txtTenHang = new JTextField();
+		txtTenHang.setBounds(105, 49, 86, 20);
+		contentPane.add(txtTenHang);
+		txtTenHang.setColumns(10);
+		
+		JLabel lblNhmHng = new JLabel("Nh\u00F3m h\u00E0ng:");
+		lblNhmHng.setBounds(22, 94, 58, 14);
+		contentPane.add(lblNhmHng);
+		
+		JComboBox cbxNhomHang = new JComboBox();
+		cbxNhomHang.setBounds(105, 91, 86, 20);
+		contentPane.add(cbxNhomHang);
+	}
+}
---
+++ DoAn/src/main/java/Model/Common.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Common {
+
+	public class CodePrefix
+    {
+        public final String SaleOrder = "PX";
+        public final String ReceiptVoucher = "PT";
+        public final String DraftSaleOrder = "TX";
+        public final String DraftReceiptVoucher = "TT";
+    }
+
+}
