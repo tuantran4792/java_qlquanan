0093ca9ebff4572c3182c9ed1fd615ab43cdc71d
Hp030792 <thphuoc0307@gmail.com>
2016-06-11 19:07:47 +0700

    Edit GUI

--- DoAn/src/main/java/Dao/CustomerDAO.java
+++ DoAn/src/main/java/Dao/CustomerDAO.java
@@ -1,83 +1,4 @@
-<<<<<<< HEAD
-package Dao;
-import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.type.DateType;
-
-import Untils.HibernateUtil;
-
-import POJO_entities.CusCustomers;
-public class CustomerDAO {
-
-	Transaction tran;
-    HibernateUtil h;
-    
-	public CustomerDAO() {
-		h = new HibernateUtil();
-		// TODO Auto-generated constructor stub
-	}
-	public CusCustomers getCustomer(long customerId)
-	{
-		CusCustomers customer = new CusCustomers();
-
-		 Session session = h.getSessionFactory().openSession();
-
-		 try {
-
-			 customer = (CusCustomers) session.get(CusCustomers.class, 
-
-		 customerId);
-
-		 } catch (HibernateException ex) {
-
-		 //Log the exception
-
-		 System.err.println(ex);
-
-		 } finally {
-
-		 session.close();
-
-		 }
-
-		 return customer;
-	}
-	
-	public long createCustomer(CusCustomers customer)
-	{
-		long result = 0;
-		Session session = h.getSessionFactory().getCurrentSession();
-		try {
-		    if(customer.getCustomerName() == null || customer.getCreatedUser() <= 0  ) 
-		    System.err.println("Co loi xay ra, xin kiem tra lai");
-		    
-			session.save(customer);
-			
-			result = customer.getId();
-		} 
-		catch (HibernateException ex) {
-			System.err.println(ex);
-		} 
-		finally {
-			session.close();
-		}
-		return result;
-	}
-	public List<CusCustomers> Search(String keyword)
-	{
-		return new ArrayList<CusCustomers>();
-	}
-}
-=======
 package Dao;
 import java.io.Serializable;
 import java.text.DateFormat;
@@ -163,4 +84,3 @@ public class CustomerDAO {
 		
 	}
 }
->>>>>>> refs/remotes/origin/master
--- DoAn/src/main/java/DoAn/TongQuan.java
+++ DoAn/src/main/java/DoAn/TongQuan.java
@@ -23,7 +23,7 @@ public class TongQuan extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtTKHangHoa, txtTKLichLV, txtTKKhachHang;
-	private JTextField txtKhachHang;
+	
 
 	/**
 	 * Launch the application.
@@ -56,119 +56,6 @@ public class TongQuan extends JFrame {
 		GlobalModel data = new GlobalModel();
 		
 		//POS
-		final JPanel pPOS = new JPanel();
-		pPOS.setBounds(166, 11, 1008, 639);
-		contentPane.add(pPOS);
-		pPOS.setLayout(null);
-		pPOS.setVisible(true);
-		
-		JLabel lblSoDoBan = new JLabel("Sơ  Đồ  Bàn");
-		lblSoDoBan.setFont(new Font("UTM French Vanilla", Font.PLAIN, 30));
-		lblSoDoBan.setBounds(73, 1, 157, 38);
-		pPOS.add(lblSoDoBan);
-		
-		JTextArea tarSoDoBan = new JTextArea();
-		tarSoDoBan.setBounds(10, 42, 283, 558);
-		pPOS.add(tarSoDoBan);
-		
-		JLabel lblHoaDon = new JLabel("Hóa  Đơn");
-		lblHoaDon.setFont(new Font("UTM French Vanilla", Font.PLAIN, 30));
-		lblHoaDon.setBounds(408, 1, 129, 38);
-		pPOS.add(lblHoaDon);
-		
-		JPanel pTTHoaDon = new JPanel();
-		pTTHoaDon.setBounds(303, 40, 343, 558);
-		pTTHoaDon.setBorder(BorderFactory.createTitledBorder("Thông tin hóa đơn"));
-		pPOS.add(pTTHoaDon);
-		pTTHoaDon.setLayout(null);
-		
-		JLabel lblSoHoaDon = new JLabel("Số hóa đơn:");
-		lblSoHoaDon.setBounds(10, 31, 68, 14);
-		pTTHoaDon.add(lblSoHoaDon);
-		
-		JLabel lblBan = new JLabel("New label");
-		lblBan.setBounds(89, 31, 77, 14);
-		pTTHoaDon.add(lblBan);
-		
-		JLabel lblNgay = new JLabel("Ngày:");
-		lblNgay.setBounds(187, 31, 45, 14);
-		pTTHoaDon.add(lblNgay);
-		
-		JLabel lblSHD = new JLabel("New label");
-		lblSHD.setBounds(242, 31, 91, 14);
-		pTTHoaDon.add(lblSHD);
-		
-		JLabel lblSoBan = new JLabel("Số bàn:");
-		lblSoBan.setBounds(10, 68, 68, 14);
-		pTTHoaDon.add(lblSoBan);
-		
-		JLabel lblSB = new JLabel("New label");
-		lblSB.setBounds(89, 68, 77, 14);
-		pTTHoaDon.add(lblSB);
-		
-		JLabel lblKhachHang = new JLabel("Khách hàng:");
-		lblKhachHang.setBounds(10, 104, 68, 14);
-		pTTHoaDon.add(lblKhachHang);
-		
-		txtKhachHang = new JTextField();
-		txtKhachHang.setBounds(89, 101, 197, 20);
-		pTTHoaDon.add(txtKhachHang);
-		txtKhachHang.setColumns(10);
-		
-		JButton btnTKKhachHang = new JButton("New button");
-		btnTKKhachHang.setBounds(297, 100, 25, 23);
-		pTTHoaDon.add(btnTKKhachHang);
-		
-		JLabel lblNhanVien = new JLabel("Nhân viên:");
-		lblNhanVien.setBounds(10, 145, 68, 14);
-		pTTHoaDon.add(lblNhanVien);
-		
-		JLabel lblTenNhanVien = new JLabel("New label");
-		lblTenNhanVien.setBounds(89, 145, 197, 14);
-		pTTHoaDon.add(lblTenNhanVien);
-		
-		JList lstDanhSach = new JList();
-		lstDanhSach.setBounds(10, 181, 323, 228);
-		pTTHoaDon.add(lstDanhSach);
-		
-		JLabel lblTongTien = new JLabel("Tổng tiền:");
-		lblTongTien.setBounds(140, 432, 58, 14);
-		pTTHoaDon.add(lblTongTien);
-		
-		JLabel lblTT = new JLabel("New label");
-		lblTT.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblTT.setBounds(209, 425, 77, 25);
-		pTTHoaDon.add(lblTT);
-		
-		JLabel lblDonViTinh = new JLabel("VNĐ");
-		lblDonViTinh.setBounds(308, 432, 25, 14);
-		pTTHoaDon.add(lblDonViTinh);
-		
-		JButton btnThanhToan = new JButton("Thanh Toán");
-		btnThanhToan.setBounds(244, 515, 89, 32);
-		pTTHoaDon.add(btnThanhToan);
-		
-		JLabel lblTienNhan = new JLabel("Tiền nhận:");
-		lblTienNhan.setBounds(140, 470, 58, 14);
-		pTTHoaDon.add(lblTienNhan);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel.setBounds(209, 461, 77, 23);
-		pTTHoaDon.add(lblNewLabel);
-		
-		JLabel lblDVT = new JLabel("VNĐ");
-		lblDVT.setBounds(308, 470, 25, 14);
-		pTTHoaDon.add(lblDVT);
-		
-		JLabel lblThucDon = new JLabel("Thực  Đơn");
-		lblThucDon.setFont(new Font("UTM French Vanilla", Font.PLAIN, 30));
-		lblThucDon.setBounds(777, 1, 129, 38);
-		pPOS.add(lblThucDon);
-		
-		JTextArea tarThucDon = new JTextArea();
-		tarThucDon.setBounds(656, 42, 337, 482);
-		pPOS.add(tarThucDon);
 		
 		//Dashboard
 		final JPanel pDashboard = new JPanel();
@@ -299,22 +186,10 @@ public class TongQuan extends JFrame {
 		contentPane.add(btnKhachHang);
 		
 		
-		btnPOS.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				pPOS.setVisible(true);
-				pDashboard.setVisible(false);
-				pHangHoa.setVisible(false);
-				pLichLamViec.setVisible(false);
-				pKhachHang.setVisible(false);
-			}
-		});
-		
 		btnDashboard.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				pDashboard.setVisible(true);
-				pPOS.setVisible(false);
 				pHangHoa.setVisible(false);
 				pLichLamViec.setVisible(false);
 				pKhachHang.setVisible(false);
@@ -325,7 +200,6 @@ public class TongQuan extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				pHangHoa.setVisible(true);
-				pPOS.setVisible(false);
 				pDashboard.setVisible(false);
 				pLichLamViec.setVisible(false);
 				pKhachHang.setVisible(false);
@@ -336,7 +210,6 @@ public class TongQuan extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				pLichLamViec.setVisible(true);
-				pPOS.setVisible(false);
 				pDashboard.setVisible(false);
 				pHangHoa.setVisible(false);
 				pKhachHang.setVisible(false);
@@ -347,7 +220,6 @@ public class TongQuan extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				pKhachHang.setVisible(true);
-				pPOS.setVisible(false);
 				pDashboard.setVisible(false);
 				pHangHoa.setVisible(false);
 				pLichLamViec.setVisible(false);
