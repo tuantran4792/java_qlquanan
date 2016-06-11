b7828e7efc057121d39f7b14f5b9beb9f4b963ca
Tuấn Trần <juriyaro@gmail.com>
2016-06-11 23:10:28 +0700

    query HangHoa

--- DoAn/src/main/java/Dao/ProductDAO.java
+++ DoAn/src/main/java/Dao/ProductDAO.java
@@ -17,25 +17,26 @@ public class ProductDAO {
 		//h = new HibernateUtil();
 	}
     
-	public List<BaseProduct> getProducts(String keyword, int CategoryID)
+    public List<BaseProduct> getProducts(String keyword, int CategoryID)
 	{
+		//keyword = "SP";
 		List<BaseProduct> result = new ArrayList<BaseProduct>();
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
 		try{
 			Criteria criteria = session.createCriteria(BaseProduct.class);
-
+			criteria.add(Restrictions.sqlRestriction("isDeleted = 0"));
+			criteria.add(Restrictions.sqlRestriction("isActived = 1"));
 			if(keyword != null && !keyword.isEmpty())
 			{
-				criteria.add(Restrictions.eq("Barcode", keyword));
+				criteria.add(Restrictions.like("barCode", "%" + keyword +"%"));
 			}
 			if(CategoryID > 0)
-				criteria.add(Restrictions.eq("CategoryID", CategoryID));
+				criteria.add(Restrictions.eq("categoryID", CategoryID));
 		    if(criteria.list().size() > 0)
 		    	result = criteria.list();
 		    }
 		catch (HibernateException ex) {
-/// FINAL TEST
 		System.err.println(ex);
 		//Log the exception
 		}
