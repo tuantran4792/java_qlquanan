c890a933bbeb7988039ad8fba62fad51aafc12af
Hp030792 <thphuoc0307@gmail.com>
2016-06-11 20:08:53 +0700

    Test 6

---
+++ DoAn/src/main/java/Dao/ProductDAO.java
@@ -0,0 +1,46 @@
+package Dao;
+
+import java.util.*;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import POJO_entities.BaseProduct;
+
+import Untils.HibernateUtil;
+
+public class ProductDAO {
+
+    public ProductDAO() {
+		//h = new HibernateUtil();
+	}
+    
+	public List<BaseProduct> getProducts(String keyword, int CategoryID)
+	{
+		List<BaseProduct> result = new ArrayList<BaseProduct>();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		try{
+			Criteria criteria = session.createCriteria(BaseProduct.class);
+
+			if(keyword != null && !keyword.isEmpty())
+			{
+				criteria.add(Restrictions.eq("Barcode", keyword));
+			}
+			if(CategoryID > 0)
+				criteria.add(Restrictions.eq("CategoryID", CategoryID));
+		    if(criteria.list().size() > 0)
+		    	result = criteria.list();
+		    }
+		catch (HibernateException ex) {
+		///ÁDADADAD
+			//ạhgshdja
+		System.err.println(ex);
+		//Log the exception
+		}
+		return result;
+	}
+	
+}
